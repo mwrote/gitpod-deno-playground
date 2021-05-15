95a6a7d4c9e0359c35de172008e21a3dd83518c8
Mwrote <yohei920@gmail.com>
2021-05-15 23:17:41 +0000

    update comment

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,5 @@
 import { ServerRequest } from "https://deno.land/std@0.96.0/http/server.ts";
 
 export default async (req: ServerRequest) => {
-	req.respond({ body: `Hello, from Deno v${Deno.version.deno}!` });
+	req.respond({ body: `こんにちわ, from Deno v${Deno.version.deno}!` });
 };
