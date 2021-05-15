9ac40a9811728c75242854e85a431c848474d2b2
Mwrote <yohei920@gmail.com>
2021-05-15 04:11:38 +0000

    add dext initial files

---
+++ deps.ts
@@ -0,0 +1,8 @@
+export { Fragment, h } from "https://deno.land/x/dext@0.10.5/deps/preact/mod.ts";
+export type {
+  AppProps,
+  GetStaticData,
+  GetStaticDataContext,
+  GetStaticPaths,
+  PageProps,
+} from "https://deno.land/x/dext@0.10.5/mod.ts";
